b36bdc57b331377b5f6c92deb9d5786c11abfadb
Illia <illia.vasiliev@gmail.com>
2023-01-16 20:30:35 +0200

    done 7.1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const divEl = document.querySelector(".gallery");
+const galleryInHTML = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<a class="gallery__link" href="${original}"><img class="gallery__image" width=340 src=${preview} data-source="${original}"alt ='${description}'></a>`;
+  })
+  .join("");
+divEl.insertAdjacentHTML("beforeend", galleryInHTML);
 
-console.log(galleryItems);
+divEl.addEventListener("click", openModal);
+
+function openModal(event) {
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  event.preventDefault();
+
+  const instance = basicLightbox.create(`
+  <img src='${event.target.dataset.source}' width="800" height="600">
+    
+`);
+
+  instance.show();
+  divEl.addEventListener("keydown", (e) => {
+    if (e.code === "Escape") {
+      instance.close();
+    }
+  });
+}
+
+const gallery = console.log(galleryItems);
