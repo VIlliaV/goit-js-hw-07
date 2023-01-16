a885bf1cb941edd6e8e33bd4e6305d385a008347
Illia <illia.vasiliev@gmail.com>
2023-01-16 21:53:59 +0200

    done 7.2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,17 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const divEl = document.querySelector(".gallery");
+const galleryInHTML = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<li><a class="gallery__link" href="${original}"><img class="gallery__image" src=${preview} alt ='${description}'></a></li>`;
+  })
+  .join("");
+divEl.insertAdjacentHTML("beforeend", galleryInHTML);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionPosition: "bottom",
+  captionsData: "alt",
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
